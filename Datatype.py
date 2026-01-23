8ef9eda5b0f8b058e553bccc3bcd38fc74d327c4
subhitgupta185-debug <subhitgupta185@gmail.com>
2026-01-23 21:48:45 +0530

    Updating changes for practice

---
+++ Datatype.py
@@ -0,0 +1,11 @@
+#string (str)
+# string represent text, they must be enclosed
+
+message = "How are you"
+name = "Rahul"
+space = " "
+
+#string can be combined (concatation)
+full_message = "Hello" + space + name + space + message 
+
+print(full_message)
